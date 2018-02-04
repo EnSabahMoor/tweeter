0793c8215b52d4b08b16e5ef7ac6321ecf4efff2
Andray <patriceomoghene@gmail.com>
2018-02-04 00:40:03 +0000

    Got the counter turning red when > 140chars

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,12 +2,16 @@ $(document).ready(function () {
     console.log("ready!");
 
     var text_max = 140;
-    $('.counter').html(text_max + ' characters remaining');
+    $('.counter').html(text_max);
 
     $('.text').keyup(function() {
-        var text_length = $('.text').val().length;
+        var text_length = $(this).val().length;
         var text_remaining = text_max - text_length;
-
+        if (text_remaining < 0) {
+            $('.counter').css('color', 'red');
+        } else {
+            $('.counter').css('color', 'black');
+        }
         $('.counter').html(text_remaining);
     });
 
